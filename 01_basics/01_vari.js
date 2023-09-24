32766f32b42d09ea9ab28ca942cbdb8480eaed19
Anil9436 <86486716+Anil9436@users.noreply.github.com>
2023-09-24 10:25:40 +0000

    Added variables and Datatypes

---
+++ 01_basics/01_vari.js
@@ -0,0 +1,8 @@
+const name = "Anil";
+
+let email = "anil@duck.com";
+
+var username = "adangudula";
+
+
+console.table([name,email,username]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict" //automatically uses new version of JS
+
+//alert() // alert is used in browser and not in node we are using nodejs
+
+
+
+let name = "anil"
+
+let age = 19
+
+let isLoggedin = true
+
+//numbers are 2 ^ 53
+//bigint
+//string => ""
+//boolean => true,false
+//null => standalone value
+//undefined is variable declared but not defiend
+//symbols
+//object
+
